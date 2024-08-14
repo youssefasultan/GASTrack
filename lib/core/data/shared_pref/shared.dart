4a929957684a8131630ceb8bf91a5201d73ab717
ECS\Youssefsu <youssef.sultan@ecs-co.com>
2024-08-14 14:43:42 +0300

    added vpn

--- lib/core/data/shared_pref/shared.dart
+++ lib/core/data/shared_pref/shared.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
---
+++ lib/core/network/open_vpn.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:openvpn_flutter/openvpn_flutter.dart';
+
+class OpenVpnService with ChangeNotifier {
+  late OpenVPN openvpn;
+  late VpnStatus status;
+  late VPNStage stage;
+
+  OpenVpnService();
+
+  void init() {
+    openvpn = OpenVPN(
+      onVpnStatusChanged: (data) {
+        status = data!;
+        debugPrint('vpn status : ${status.toString()}');
+      },
+      onVpnStageChanged: (stage, rawStage) {
+        this.stage = stage;
+        notifyListeners();
+        debugPrint('vpn stage: ${stage.toString()}');
+      },
+    );
+
+    openvpn.initialize(
+      groupIdentifier: "com.example.gastech_app",
+      localizedDescription: 'GasTrackVPN',
+    );
+  }
+
+  void connect() async {
+    try {
+      var content = await rootBundle
+          .loadString('assets/vpn/pfsense-TCP4-6443-gastech-config.ovpn');
+      if (openvpn.initialized) {
+        openvpn.connect(
+          content,
+          'GASTEC VPN',
+          username: 'gastech-user02',
+          password: 'y9fKH?1>}46Q',
+          certIsRequired: true,
+        );
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  void disconnect() {
+    openvpn.disconnect();
+  }
+}
